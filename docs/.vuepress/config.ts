75cd6f51d7c79923d4332a2af97dae1cbeda2436
Ivan <ivan.gao@cnosdb.com>
2022-10-25 15:03:31 +0800

    set chinese as the preferred language

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -54,12 +54,12 @@ export default defineUserConfig({
   ],
 
   locales: {
-    '/zh/': {
+    '/': {
       lang: 'zh-CN',
       title: '文档',
       description: 'CnosDB 的文档',
     },
-    '/': {
+    '/en/': {
       lang: 'en-US',
       title: 'Docs',
       description: 'A docs for CnosDB',
@@ -78,7 +78,7 @@ export default defineUserConfig({
   plugins: [
     searchPlugin({
       locales: {
-        '/zh/': {
+        '/zh': {
           placeholder: '搜索',
         },
       },
---
+++ docs/en/README.md
@@ -0,0 +1,13 @@
+---
+icon: home
+title: CnosDB
+heroImage: /logo.png
+heroText: CnosDB
+tagline: CnosDB
+actions:
+- text: Docs
+link: /guide/
+type: primary
+
+copyright: false
+---
