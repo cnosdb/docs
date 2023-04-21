a4924460799fa04c8765d46c33009936032c9de9
heshimang <hsm15031585502@163.com>
2023-04-21 11:05:50 +0800

    fix: fixed console error

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress';
 import theme from './theme.js';
 import { searchPlugin } from '@vuepress/plugin-search';
 import { getDirname, path } from '@vuepress/utils';
+import { docsearchPlugin } from '@vuepress/plugin-docsearch';
 
 const __dirname = getDirname(import.meta.url);
 
@@ -17,20 +18,22 @@ export default defineUserConfig({
       'script',
       {},
       `
-        (function() {
-          var logoEleLight = document.querySelector(".navbar-left a.brand .logo.light") ;
-          var logoEleDark = document.querySelector(".navbar-left a.brand .logo.dark") ;
-          
-          logoEleLight.href = "https://www.cnosdb.com";
-          logoEleDark.href = "https://www.cnosdb.com";
-          
-          logoEleLight.addEventListener('click', function() {
-            window.location.href = 'https://www.cnosdb.com'
-          })
-          logoEleDark.addEventListener('click', function() {
-            window.location.href = 'https://www.cnosdb.com'
-          })
-        })();
+        window.onload = function() {
+          (function() {
+            var logoEleLight = document.querySelector(".navbar-left a.brand .logo.light") ;
+            var logoEleDark = document.querySelector(".navbar-left a.brand .logo.dark") ;
+            
+            logoEleLight.href = "https://www.cnosdb.com";
+            logoEleDark.href = "https://www.cnosdb.com";
+            
+            logoEleLight.addEventListener('click', function() {
+              window.location.href = 'https://www.cnosdb.com'
+            })
+            logoEleDark.addEventListener('click', function() {
+              window.location.href = 'https://www.cnosdb.com'
+            })
+          })();
+        }   
       `,
     ],
     [
@@ -45,13 +48,6 @@ export default defineUserConfig({
           s.parentNode.insertBefore(hm, s);
         })();
       `,
-    ],
-    [
-      'script',
-      {
-        defer: true,
-        src: path.resolve(__dirname, './components/jQuery.js'),
-      },
     ],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
   ],
@@ -81,10 +77,51 @@ export default defineUserConfig({
     };
   },
   plugins: [
-    searchPlugin({
+    docsearchPlugin({
+      appId: 'UT8LB08QUO',
+      apiKey: 'e4c71a1ddb01bd783f3a822e6ce5519e',
+      indexName: 'prod_cnosdb',
       locales: {
-        '/zh': {
-          placeholder: '搜索',
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
+              },
+            },
+          },
         },
       },
     }),
