2c3a161a30d6b486b09aeaa8fe5ba41cbd3c834a
Ivan <ivan.gao@cnosdb.com>
2023-04-28 16:13:37 +0800

    add v2.3 preset position.

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -3,7 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
     {
         text: '版本',
-        link: 'latest' ,
+        link: '版本' ,
         children: [
             { text: 'latest', link: '/zh/latest/' },
             { text: 'v2.3', link: '/zh/v2.3/' },
