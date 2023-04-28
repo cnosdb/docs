bcfa3cddf9d53bcceeb2d08d6dcddeea6c4abc20
Ivan <ivan.gao@cnosdb.com>
2023-04-28 16:13:37 +0800

    add v2.3 preset position.

--- docs/.vuepress/navbar/en.ts
+++ docs/.vuepress/navbar/en.ts
@@ -3,7 +3,7 @@ import { navbar } from 'vuepress-theme-hope';
 export const enNavbar = navbar([
     {
         text: 'Version',
-        link: 'latest' ,
+        link: 'Version' ,
         children: [
             { text: 'latest', link: '/en/latest/' },
             { text: 'v2.3', link: '/en/v2.3/' },
---
+++ docs/zh/v2.3/README.md
@@ -0,0 +1,10 @@
+---
+icon: home
+title: 主页
+order: -8
+index: false
+---
+
+# Home
+
+## 敬请期待！！！
