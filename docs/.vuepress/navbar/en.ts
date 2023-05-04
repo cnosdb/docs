e968e14de06bfd83c1974ff6be1b62ddbc427dd9
Ivan <ivan.gao@cnosdb.com>
2023-05-04 18:39:28 +0800

    chore: add cloud document content.

--- docs/.vuepress/navbar/en.ts
+++ docs/.vuepress/navbar/en.ts
@@ -2,11 +2,16 @@ import { navbar } from 'vuepress-theme-hope';
 
 export const enNavbar = navbar([
     {
-        text: 'Version',
-        link: 'Version' ,
+        text: 'CnosDB',
+        link: 'CnosDB',
         children: [
             { text: 'latest', link: '/en/latest/' },
             { text: 'v2.3', link: '/en/v2.3/' },
         ],
+    },
+
+    {
+        text: 'Cloud',
+        link: '/en/cloud/' ,
     },
 ]);
