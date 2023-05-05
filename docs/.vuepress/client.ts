cbc9125c2c24acb8d28fa3ddec6c4f4303de6e4f
heshimang <hsm15031585502@163.com>
2023-05-05 15:19:40 +0800

    fix: fix build error

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -27,32 +27,33 @@ const verifyToken = (token: string) => {
       return ctx;
     },
   });
-  console.log('data', data);
 };
 
 export default defineClientConfig({
   enhance({ router }) {
-    router.beforeEach((to, _from, next) => {
-      console.log('before navigation', to);
-      if (to.path === '/') {
-        next({ path: '/en/latest/' });
-      } else if (to.path === '/en/') {
-        next({ path: '/en/latest/' });
-      } else if (to.path === '/zh/') {
-        next({ path: '/zh/latest/' });
-      } else if (to.path.indexOf('/en/cloud/') !== -1) {
-        // 说明这里匹配到了cloud的文档，需要鉴权，判断url的query是否有token，如果有则正常调用校验接口，否则去cloud登录
-        const query_token = to.query.token;
-        query_token &&
-          window.sessionStorage.setItem('cloud_token', query_token as string);
-        const token = window.sessionStorage.getItem('cloud_token');
-        if (token) {
-          verifyToken(token);
-        } else {
-          window.location.href = 'https://cnosdb.cloud/start?callback=docs';
+    if (typeof window === 'object') {
+      router.beforeEach((to, _from, next) => {
+        console.log('before navigation', to);
+        if (to.path === '/') {
+          next({ path: '/en/latest/' });
+        } else if (to.path === '/en/') {
+          next({ path: '/en/latest/' });
+        } else if (to.path === '/zh/') {
+          next({ path: '/zh/latest/' });
+        } else if (to.path.indexOf('/en/cloud/') !== -1) {
+          // 说明这里匹配到了cloud的文档，需要鉴权，判断url的query是否有token，如果有则正常调用校验接口，否则去cloud登录
+          const query_token = to.query.token;
+          query_token &&
+            window.sessionStorage.setItem('cloud_token', query_token as string);
+          const token = window.sessionStorage.getItem('cloud_token');
+          if (token) {
+            verifyToken(token);
+          } else {
+            window.location.href = 'https://cnosdb.cloud/start?callback=docs';
+          }
         }
-      }
-      next();
-    });
+        next();
+      });
+    }
   },
 });
