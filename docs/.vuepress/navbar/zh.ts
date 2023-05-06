7df444607edbab07d91a706776b9bfe1f699ad25
Ivan <ivan.gao@cnosdb.com>
2023-05-06 15:56:08 +0800

    feat: add enterprise ui entry.

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -2,12 +2,17 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
     {
-        text: '版本',
-        link: '版本' ,
+        text: 'CnosDB',
+        link: 'CnosDB' ,
         children: [
             { text: 'latest', link: '/zh/latest/' },
             { text: 'v2.3', link: '/zh/v2.3/' },
         ]
+    },
+
+    {
+        text: 'Enterprise-UI',
+        link: '/zh/enterprise-ui/' ,
     },
 
 ]);
