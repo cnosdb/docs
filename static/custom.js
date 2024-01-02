175aa263a0c97e5d5e10b4862a22cddfbe85d7ab
heshimang <hsm15031585502@163.com>
2024-01-02 16:26:34 +0800

    feat: rm console

--- static/custom.js
+++ static/custom.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   const handleCloudBtn = () => {
     const cloud = document.querySelector('.navbar__item.nav-cloud');
-    console.log('cloud', cloud);
     const isEnEnv = window.location.href.indexOf('en') > -1;
     cloud.style.display = isEnEnv ? 'block' : 'none';
   };
